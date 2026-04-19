2825cd856a61c38a15aa6e96229fb43129a80a67
g-class857 <halbsry340@gmail.com>
2026-04-19 13:13:41 +0300

    Update train_XGBOOST.py

--- models/train_XGBOOST.py
+++ models/train_XGBOOST.py
@@ -77,7 +77,8 @@ scale_pos_weight = neg / pos # negative to positive ratio
 
 print("Positives:", pos)
 print("Negatives:", neg)
-print("scale_pos_weight:", scale_pos_weight) # tells XGBoost to weight positive class more heavily when >1.
+print("scale_pos_weight:", scale_pos_weight) # tells XGBoost to weight positive class more heavily when >1, increase the volume so the model can hear it over the noise of the majority.
+# shift the model focus from maximizing accuracy (useless for imbalanced data) to maximizing precision and recall (find actual spam) 
 # ==========================================================
 # Base XGBoost parameters
 # ==========================================================
@@ -91,7 +92,7 @@ BASE_PARAMS = {
     "nthread": N_THREADS,
     
     "max_bin": 128, # histogtam bins 
-    "scale_pos_weight": scale_pos_weight # as computed above
+    "scale_pos_weight": scale_pos_weight # as computed above. "Every time you miss a Spam email, I'm going to charge you 9 penalty points instead of 1." to force the model to learn the actual pattern
 }
 # ==========================================================
 # Optuna objective with native XGBoost CV
@@ -106,48 +107,49 @@ OPTUNA_TIMEOUT_SECONDS = 60 * 60 * 4  # 3 hours max for tuning run (optional)
 def objective(trial): # called by optuna for each huperparameter combination
     # =========================
     # Sample subset (IMPORTANT)
-    # =========================
-    rng = np.random.RandomState(42 + trial.number) # use different random seed for trial
+    # ========================= # 40k rows x 100 trials = 4M rows 
+    rng = np.random.RandomState(42 + trial.number) # use different random seed for trial to see the different data each trial to work generally not just on specific subset
     subset_size = min(40000, X_train.shape[0]) # takes at mose 40,000 rows 
-    idx = rng.choice(X_train.shape[0], subset_size, replace=False) # randomly samples without replacement
+    idx = rng.choice(X_train.shape[0], subset_size, replace=False) # randomly samples without replacement that guarantees the rows are unique not train on the same row twice in the same batch
+    # X_train.shape[0] : num of available rows
     # this reduces tuning time and memory
-    X_sub = X_train[idx]
-    y_sub = y_train[idx] 
+    X_sub = X_train[idx] # pass the list, numpy  grabs exactly those rows 
+    y_sub = y_train[idx] # use idx in emails and labels to ensure perfectly aligning
     # =========================
     # Define parameters
     # =========================
     params = BASE_PARAMS.copy()
 
     params.update({
-        "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.15, log=True), # samples on logarithmic scale, float for (learning rate, regularization)
-        "max_depth": trial.suggest_int("max_depth", 4, 10), 
-        "subsample": trial.suggest_float("subsample", 0.6, 1.0), # fraction of rows used per tree 
-        "colsample_bytree": trial.suggest_float("colsample_bytree", 0.6, 1.0), # fraction of features used per tree
-        "min_child_weight": trial.suggest_int("min_child_weight", 1, 15), # minimum sum of instance weights in a child 
-        "gamma": trial.suggest_float("gamma", 0.0, 5.0), # minimum loss reduction required to split
-        "lambda": trial.suggest_float("lambda", 1e-3, 10.0, log=True), # L2 regularization
-        "alpha": trial.suggest_float("alpha", 1e-3, 10.0, log=True), # L1 regularization
-    })
+        "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.15, log=True), # multiply it by prediction of a tree defore adding to total. + learns faster but might overshoot optimal solution, - makes tiny contribution need more trees but robusr and smooth. samples on logarithmic scale, float for (learning rate, regularization). log ensures tuner to spend equal time exploring the mathematically sensitive small numbers
+        "max_depth": trial.suggest_int("max_depth", 4, 10), # maximum number of splits, + overfitting , - underfitting
+        "subsample": trial.suggest_float("subsample", 0.6, 1.0), # fraction of rows used per tree. ensures generalization
+        "colsample_bytree": trial.suggest_float("colsample_bytree", 0.6, 1.0), # fraction of fetures used per tree, not relying on specific features, generalization
+        "min_child_weight": trial.suggest_int("min_child_weight", 1, 15), # minimum amount of "data weights" required to creat new leaf. creat new leaf on what value? 
+        "gamma": trial.suggest_float("gamma", 0.0, 5.0), # minimum loss reduction required to split. if the improvement of the split less than gamma abort the split
+        "lambda": trial.suggest_float("lambda", 1e-3, 10.0, log=True), # L2 regularization, penalizes outlier weights to prevent single tree branch from too much impacting over prediction
+        "alpha": trial.suggest_float("alpha", 1e-3, 10.0, log=True), # L1 regularization, ignores less important features, setting them to zero
+    }) # lambda and alpha are mathematical penalties applied to final weights of the leaves
     # =========================
     # Create DMatrix
     # =========================
-    dtrain = xgb.DMatrix(X_sub, label=y_sub, nthread=2) # xgboost internal data structure
+    dtrain = xgb.DMatrix(X_sub, label=y_sub, nthread=2) #  highly optimized data format, it converts standard pandas/numpy data into a specialized memory structure that XGBoost can process blazingly fast.
     # =========================
     # Cross Validation
     # =========================
     cv = xgb.cv( # performs cross‑validation and returns a DataFrame of metrics.
         params=params,
         dtrain=dtrain,
-        num_boost_round=600, # uses the tuning‑specific lower value.
+        num_boost_round=600, # don't build more than 600 trees 
         nfold=3,
-        stratified=True, # preserves class proportions in each fold.
-        early_stopping_rounds=25,
+        stratified=True, # maintains exact ratio on evey fold
+        early_stopping_rounds=25, # stop after 25 trees built if no improvement, prevent overfitting
         seed=42,
         verbose_eval=False
-    )
+    ) 
 
     best_score = cv["test-aucpr-mean"].max() 
-    #  column with best value of mean AUC‑PR across folds 
+    #  rely on best value of mean AUC‑PR across folds instead of accuracy since the data is imbalance
     # =========================
     # Memory cleanup (CRITICAL)
     # =========================
@@ -162,9 +164,9 @@ study = optuna.create_study(
     storage="sqlite:///optuna_phishguard.db", # save study result for persistence in sqlite db
     load_if_exists= True, # resumes from previous  
     direction="maximize", # we wanna maximize 
-    sampler=optuna.samplers.TPESampler(seed=RANDOM_STATE), # Tree‑structured Parzen Estimator (default, good for hyperparameter search).
-    pruner=MedianPruner(n_startup_trials=5, n_warmup_steps=5), # prunes trials that perform below the median of completed trials after 5 warm‑up steps.
-)
+    sampler=optuna.samplers.TPESampler(seed=RANDOM_STATE), # Tree‑structured Parzen Estimator (default, good for hyperparameter search). Bayesian optimization(looks back, clusters to good and bad, then samples the next trial from high probability area) gets smarter over time by focus on best settings 
+    pruner=MedianPruner(n_startup_trials=5, n_warmup_steps=5), # prunes trials that perform below the median of completed trials after 5 warm‑up steps. 
+) # n_startup_trials=5: Let at least 5 trials finish completely so the tuner knows what a "normal" score looks like. n_warmup_steps=5: Let every new trial run for at least 5 rounds (trees) before judging it.
 best_params = study.best_trial.params
 print("Best parameters (from subset tuning):", best_params)
 
@@ -235,10 +237,10 @@ print("Recall at best:", recall[best_idx])
 # Save SHAP background sample
 # ==========================================================
 
-sample_size = min(1024, X.shape[0]) # takes up to 1024 random rows from the full dataset
+sample_size = min(1024, X.shape[0]) # takes up to 1024 random rows from the full dataset to represent the normal state of data
 rng = np.random.RandomState(RANDOM_STATE)
 idx = rng.choice(X.shape[0], sample_size, replace=False)
-bg = X[idx]
+bg = X[idx] 
 np.save(SHAP_BACKGROUND_PATH, bg) # save as npy
 print("SHAP background saved")
 # ==========================================================
